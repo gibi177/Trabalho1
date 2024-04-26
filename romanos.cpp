8aa6e9afa9c2253078f59969aa2c05ae96f7b861
felipe gibin <gibinf.duarte@gmail.com>
2024-04-26 19:30:50 -0300

    Adequa algumas recomendacoes do cpplint

--- romanos.cpp
+++ romanos.cpp
@@ -5,17 +5,16 @@
 #include <regex>
 
 
-//Checa validade usando expressoes regulares
+// Checa validade usando expressoes regulares
 bool check_invalid_expression(std::string romano) {
     std::regex pattern("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
 
-    return regex_match(romano, pattern); 
+    return regex_match(romano, pattern);
 }
 
 
 int romano_para_decimal(const std::string& romano) {
-
-    //Verifica string vazia ou expressao invalida
+    // Verifica string vazia ou expressao invalida
     if (romano.empty() || !check_invalid_expression(romano)) {
         return -1;
     }
@@ -30,17 +29,17 @@ int romano_para_decimal(const std::string& romano) {
     int result = 0;
     int size = romano.size();
 
-    for (int i = 0; i < size; ++i) {                                //++i incrementa a contadora i
+    for (int i = 0; i < size; ++i) {                                // ++i incrementa a contadora i
         char current = romano[i];
 
-        if (validChars.find(current) == validChars.end()){          //Se nao achar caractere atual
-            return -1;                                              //caractere atual nao e romano valido
-        }
-        
-        int value = valores[romano[i]];                             //Ex: XXI, quando i=0, value=10
-        
-        if (i + 1 < size && valores[romano[i + 1]] > value) {       //Se prox char existe e é maior que o anterior
-            result -= value; 
+        if (validChars.find(current) == validChars.end()) {         // Se nao achar caractere atual
+            return -1;                                              // caractere atual nao e romano valido
+        }  
+
+        int value = valores[romano[i]];                             // Ex: XXI, quando i=0, value=10
+                                     
+        if (i + 1 < size && valores[romano[i + 1]] > value) {       // Se prox char existe e é maior que o anterior
+            result -= value;
         } else {
             result += value;
         }
